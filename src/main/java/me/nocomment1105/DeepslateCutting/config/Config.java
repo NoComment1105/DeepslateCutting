e0b80cbdcc092985b8d331add19acadc7fa89b4f
NoComment1105 <NoComment1105@outlook.com>
2021-07-22 08:45:09 +0100

    new: Add a config system to turn the deepslate stuff on and off

---
+++ src/main/java/me/nocomment1105/DeepslateCutting/config/Config.java
@@ -0,0 +1,31 @@
+package me.nocomment1105.DeepslateCutting.config;
+
+import com.moandjiezana.toml.Toml;
+import me.nocomment1105.DeepslateCutting.deepslatecutting;
+import net.fabricmc.loader.api.FabricLoader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Objects;
+
+
+public class Config {
+    public static Config instance;
+    private boolean smoothStuff;
+    public Config(boolean b){
+        this.smoothStuff = b;
+        File file = FabricLoader.getInstance().getConfigDir().resolve("deepslatecutting.toml").toFile();
+        if(file.exists() && file.isFile()) {
+            Toml toml = new Toml().read(file);
+            this.smoothStuff = toml.getBoolean("smoothStuff");
+        } else {
+            deepslatecutting.LOGGER.info("Unable to find config file for DeepslateCutting, creating");
+            try {
+                Files.copy(Objects.requireNonNull(Config.class.getResourceAsStream("/data/deepslatecutting/default_config.toml")), file.toPath());
+            } catch (IOException e) {
+                deepslatecutting.LOGGER.warn("Unable to create config file for DeepslateCutting");
+            }
+       }
+    }
+    public boolean getSmoothStuff() {return smoothStuff;}
+}
