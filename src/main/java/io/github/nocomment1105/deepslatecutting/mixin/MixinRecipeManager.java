b5f077b66d735750558fa6dcea716f5b6b6e5a96
NoComment <NoComment1105@outlook.com>
2021-08-11 07:10:00 +0100

    new: Add some comments

--- src/main/java/io/github/nocomment1105/deepslatecutting/mixin/MixinRecipeManager.java
+++ src/main/java/io/github/nocomment1105/deepslatecutting/mixin/MixinRecipeManager.java
@@ -17,14 +17,18 @@ import static io.github.nocomment1105.deepslatecutting.DeepslateCutting.*;
 
 @Mixin(RecipeManager.class)
 public abstract class MixinRecipeManager {
+    // Mixin to RecipeManager#apply to disable the recipes within the "extras" directory
     @Inject(method = "apply", at = @At("HEAD"))
+    // TODO wtf does any of this mean
     private void apply(Map<Identifier, JsonElement> map, ResourceManager resourceManager, Profiler profiler, CallbackInfo callbackInfo) {
         Iterator<Map.Entry<Identifier, JsonElement>> iterator = map.entrySet().iterator();
 
         while (iterator.hasNext()) {
             Identifier id = iterator.next().getKey();
-
-            if (id.getNamespace().equals(MOD_ID) && id.getPath().startsWith("extras") && !CONFIG.getSmoothStuff()) {
+            // id.getPath().startsWith("extras") ignores recipes the "extras" recipe sub-directory.
+            // That is then controlled by !CONFIG.getSmoothStuff() which checks the config value for true or false
+            if (id.getNamespace().equals(MOD_ID) && id.getPath().startsWith("extras") && !CONFIG.smoothStuff) {
+                // simply removes the recipes.
                 iterator.remove();
             }
         }
