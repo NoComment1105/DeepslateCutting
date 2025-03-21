2a2a1e47b018392bc321b3ab524ee1b80320a8f5
NoComment <nocomment1105@outlook.com>
2025-03-21 19:35:59 +0000

    Update to 1.21.4

--- common/src/main/java/io/github/nocomment1105/deepslatecutting/DeepslateCuttingMain.java
+++ common/src/main/java/io/github/nocomment1105/deepslatecutting/DeepslateCuttingMain.java
@@ -3,6 +3,10 @@ package io.github.nocomment1105.deepslatecutting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class DeepslateCuttingMain {
+public final class DeepslateCuttingMain {
     public static final String MOD_ID = "deepslatecutting";
+
+    public static void init() {
+
+    }
 }
--- neoforge/src/main/java/io/github/nocomment1105/deepslatecutting_neoforge/DeepslateCutting.java
+++ neoforge/src/main/java/io/github/nocomment1105/deepslatecutting_neoforge/DeepslateCutting.java
@@ -1,9 +1,9 @@
-package io.github.nocomment1105.deepslatecutting_forge;
+package io.github.nocomment1105.deepslatecutting_neoforge;
 
 import io.github.nocomment1105.deepslatecutting.DeepslateCuttingMain;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 
-@Mod(DeepslateCuttingMain.MOD_ID + "_forge")
+@Mod(DeepslateCuttingMain.MOD_ID + "_neoforge")
 public class DeepslateCutting {
     public DeepslateCutting() {
     }
